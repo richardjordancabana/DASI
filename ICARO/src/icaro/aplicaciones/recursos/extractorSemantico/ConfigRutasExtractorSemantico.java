1f8571609a13d3afbe87a84a864bbfb8185b4a2e
richardjordancabana <pichipichi310@hotmail.com>
2015-04-12 23:30:15 +0200

    prueba1

--- ICARO/src/icaro/aplicaciones/recursos/extractorSemantico/ConfigRutasExtractorSemantico.java
+++ ICARO/src/icaro/aplicaciones/recursos/extractorSemantico/ConfigRutasExtractorSemantico.java
@@ -13,6 +13,6 @@ package icaro.aplicaciones.recursos.extractorSemantico;
 public class ConfigRutasExtractorSemantico {
     public static final String DirectorioGateHome= "C:\\GATE_Developer_8.0";
 //    public static final String DirectorioAppFile= "E:\\FicheroRed\\GatePruebas\\anniePruebaEjemplo1";
-    public static final String DirectorioAppFile= "C:\\prototipo\\iMedic-master\\anniePruebaEjemplo1";
+    public static final String DirectorioAppFile= "C:\\prototipoDASI\\ICARO\\anniePruebaEjemplo1";
     
 }
