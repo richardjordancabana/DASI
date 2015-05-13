83406994fe55ffde1ef799094580080dd2e88e21
Daniel A. Argüelles Casas <daniel@ucm-121-205.wifi.ucm.es>
2015-05-13 18:40:04 +0200

    #1 -> Inicio del proyecto

---
+++ ICAROChattersConInfrastrV0_2/src/icaro/aplicaciones/agentes/AgenteAplicacionDasi/tareas/NombreAplicacion.java
@@ -0,0 +1,59 @@
+/*
+ * Descripción: Envía un mensaje con una lista de posibles aplicaciones.
+ */
+package icaro.aplicaciones.agentes.AgenteAplicacionDasi.tareas;
+
+import icaro.aplicaciones.agentes.AgenteAplicacionDasi.objetivos.ContestarUsuario;
+// import icaro.aplicaciones.informacion.gestionCitas.VocabularioGestionCitas;
+import icaro.aplicaciones.recursos.comunicacionChat.ItfUsoComunicacionChat;
+import icaro.infraestructura.entidadesBasicas.NombresPredefinidos;
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.CausaTerminacionTarea;
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.Focus;
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.Objetivo;
+import icaro.infraestructura.entidadesBasicas.procesadorCognitivo.TareaSincrona;
+import icaro.infraestructura.recursosOrganizacion.recursoTrazas.imp.componentes.InfoTraza;
+import icaro.aplicaciones.informacion.gestionCitas.VocabularioGestionCitas;
+import icaro.aplicaciones.informacion.Dasi.VocabularioDasi;
+/**
+ *
+ * @author Javier Longo
+ */
+public class NombreAplicacion extends TareaSincrona{
+     private Objetivo contextoEjecucionTarea = null;
+  @Override
+	public void ejecutar(Object... params) {
+  /**
+   * Produce un saludo inicial y una presentacion de funcionalidad inicial al entrar en el sistema
+   */
+   String identDeEstaTarea=this.getIdentTarea();
+            String identAgenteOrdenante = this.getIdentAgente();
+          String identInterlocutor = (String)params[0];
+                    try {
+//         // Se busca la interfaz del recurso en el repositorio de interfaces 
+		ItfUsoComunicacionChat recComunicacionChat = (ItfUsoComunicacionChat) NombresPredefinidos.REPOSITORIO_INTERFACES_OBJ.obtenerInterfazUso(
+						VocabularioGestionCitas.IdentRecursoComunicacionChat);          
+                if (recComunicacionChat!=null){
+                    recComunicacionChat.comenzar(identAgenteOrdenante);
+                    String mensajeAenviar = VocabularioDasi.RESPUESTA_APLICACION;
+                    recComunicacionChat.enviarMensagePrivado(mensajeAenviar);
+                }
+                
+                
+                
+                
+                else {
+                    identAgenteOrdenante = this.getAgente().getIdentAgente();
+                     this.generarInformeConCausaTerminacion(identDeEstaTarea, contextoEjecucionTarea, identAgenteOrdenante, "Error-AlObtener:Interfaz:"+
+                             VocabularioGestionCitas.IdentRecursoComunicacionChat, CausaTerminacionTarea.ERROR);
+                        }
+                    } catch(Exception e) {
+                        this.generarInformeConCausaTerminacion(identDeEstaTarea, contextoEjecucionTarea, identAgenteOrdenante, "Error-Acceso:Interfaz:"+
+                                VocabularioGestionCitas.IdentRecursoComunicacionChat, CausaTerminacionTarea.ERROR);
+			e.printStackTrace();
+		}
+	}
+  
+
+}
+
+
